18e8daf8afa680be5c64cf8293abf8216872477d
ruiwarn <ruiwarn@gmail.com>
2026-02-07 12:16:09 +0800

    🔧chore(cleanup): 删除不必要的技能和相关文件
    - 移除 .system/.codex-system-skills.marker 文件。
    - 删除 .system/skill-creator/SKILL.md 文件，以及其中的所有内容。
    - 清理 .system/skill-creator 中的多个脚本和依赖文件，包括 init_skill.py、package_skill.py 和 quick_validate.py。
    - 删除 .system/skill-installer 目录中的 LICENSE.txt 和 SKILL.md 文件。
    - 移除 algorithmic-art 目录及其相关的 LICENSE.txt 和 SKILL.md 文件，以及所有脚本和模板文件。

--- npx_install.sh
+++ npx_install.sh
@@ -3,8 +3,28 @@
 
 npx skills add https://github.com/ruiwarn/skills
 
+#算法哲学是通过代码表达的计算美学运动。输出文件有 .md 文件（哲学）、.html 文件（交互式查看器）和 .js 文件（生成算法）。
+npx skills add https://github.com/anthropics/skills --skill algorithmic-art
+
+#提供一个系统化的代码审查清单。这个技能可以帮助审阅者确保代码质量，发现 bug，识别安全问题，并在整个代码库中保持一致性。
+npx skills add https://github.com/sickn33/antigravity-awesome-skills --skill code-review-checklist
+
+#此技能指导代理对本地开发和远程提交的代码进行专业且彻底的代码审查。
+npx skills add https://github.com/google-gemini/gemini-cli --skill code-reviewer
+
+#此技能提供了一个结构化的工作流程，用于指导用户协作创建文档。充当主动的指导者，用户通过上下文收集、细化和结构化以及读者测试三个阶段进行指导。
+npx skills add https://github.com/anthropics/skills --skill doc-coauthoring
+
+#操作docx\pdf\pptx文件的技能，theme-factory技能提供了一个PPT系统化的方法来创建和管理文档主题，确保文档在视觉上具有吸引力和一致性。
+npx skills add https://github.com/anthropics/skills --skill docx xlsx pdf pptx theme-factory
+
+#这项技能指导创建独特的、生产级的前端界面，避免使用通用的“AI 滑稽”美学。在实现真正的工作代码时，对美学细节和创意选择给予特别的关注。
+npx skills add https://github.com/anthropics/skills --skill frontend-design
+
+#noteboolm访问
 npx skills add https://github.com/PleasePrompto/notebooklm-skill
 
+#Superpowers 是一套可组合的“技能”和一些初始指令，用于构建用于编码代理的完整软件开发工作流程，确保代理使用这些技能。
 npx skills add https://github.com/obra/superpowers --skill requesting-code-review
 
-npx skills add https://github.com/google-gemini/gemini-cli --skill code-reviewer
+
